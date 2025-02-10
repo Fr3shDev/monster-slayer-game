02de35d01f709dfb22c3c043c352c5c306859ede
Fresh Dev <billyhans90@gmail.com>
2025-02-10 09:59:19 +0100

    Output text and create first package

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
+import "github.com/Fr3shDev/monster-slayer-game/interaction"
+
 func main() {
-	
+	startGame()
+
+	winner := "" // "Player" || "Monster" || ""
+
+	for winner == "" {
+		winner = executeRound()
+	}
+
+	endGame()
 }
+
+func startGame() {
+	interaction.PrintGreeting()
+}
+
+func executeRound() string {}
+
+func endGame() {}
